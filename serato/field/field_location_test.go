71ddc960f94952be2555260c22fbcfd89a347058
Tom Bell <tomb@tomb.io>
2018-11-02 01:38:22 +0000

    Add Location field and unit test stubs
    This field appears to always be empty when I include it in the history
    in Serato DJ Pro.

---
+++ serato/field/field_location_test.go
@@ -0,0 +1,25 @@
+package field_test
+
+import "testing"
+
+// XXX: Location field appears to always be empty in session files.
+
+func TestNewLocationField(t *testing.T) {
+	t.Skip()
+}
+
+func TestNewLocationFieldUnexpectedEOF(t *testing.T) {
+	t.Skip()
+}
+
+func TestNewLocationFieldUnexpectedIdentifier(t *testing.T) {
+	t.Skip()
+}
+
+func TestLocationValue(t *testing.T) {
+	t.Skip()
+}
+
+func TestLocationString(t *testing.T) {
+	t.Skip()
+}
