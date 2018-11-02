c16b14241fbf4edeb38841109c9709107f017194
Tom Bell <tomb@tomb.io>
2018-11-02 02:20:24 +0000

    Add Filename field and unit test stubs
    This field appears to always be empty when I include it in the history
    in Serato DJ Pro.

---
+++ serato/field/field_filename_test.go
@@ -0,0 +1,25 @@
+package field_test
+
+import "testing"
+
+// XXX: Filename field appears to always be empty in session files.
+
+func TestNewFilenameField(t *testing.T) {
+	t.Skip()
+}
+
+func TestNewFilenameFieldUnexpectedEOF(t *testing.T) {
+	t.Skip()
+}
+
+func TestNewFilenameFieldUnexpectedIdentifier(t *testing.T) {
+	t.Skip()
+}
+
+func TestFilenameValue(t *testing.T) {
+	t.Skip()
+}
+
+func TestFilenameString(t *testing.T) {
+	t.Skip()
+}
