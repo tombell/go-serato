6f8c8fcb724d9bfdb73e5816dd62ed13fe9583ed
Tom Bell <tomb@tomb.io>
2018-11-04 14:59:49 +0000

    Add Bitrate field and unit tests

---
+++ serato/field/field_bitrate_test.go
@@ -0,0 +1,25 @@
+package field_test
+
+import "testing"
+
+// XXX: Bitrate field appears to always be empty in session files.
+
+func TestNewBitrateField(t *testing.T) {
+	t.Skip()
+}
+
+func TestNewBitrateFieldUnexpectedEOF(t *testing.T) {
+	t.Skip()
+}
+
+func TestNewBitrateFieldUnexpectedIdentifier(t *testing.T) {
+	t.Skip()
+}
+
+func TestBitrateValue(t *testing.T) {
+	t.Skip()
+}
+
+func TestBitrateString(t *testing.T) {
+	t.Skip()
+}
