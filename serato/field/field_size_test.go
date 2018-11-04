b2af6861ab52fa13a75085faa791d9133e709fa6
Tom Bell <tomb@tomb.io>
2018-11-04 13:11:09 +0000

    Add Size field and unit tests

---
+++ serato/field/field_size_test.go
@@ -0,0 +1,25 @@
+package field_test
+
+import "testing"
+
+// XXX: Size field appears to always be empty in session files.
+
+func TestNewSizeField(t *testing.T) {
+	t.Skip()
+}
+
+func TestNewSizeFieldUnexpectedEOF(t *testing.T) {
+	t.Skip()
+}
+
+func TestNewSizeFieldUnexpectedIdentifier(t *testing.T) {
+	t.Skip()
+}
+
+func TestSizeValue(t *testing.T) {
+	t.Skip()
+}
+
+func TestSizeString(t *testing.T) {
+	t.Skip()
+}
