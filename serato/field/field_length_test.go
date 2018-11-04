bc10d85cbb08dacedcf2d2d91d437054289f92cb
Tom Bell <tomb@tomb.io>
2018-11-04 13:05:42 +0000

    Add Length field and unit tests

---
+++ serato/field/field_length_test.go
@@ -0,0 +1,25 @@
+package field_test
+
+import "testing"
+
+// XXX: Length field appears to always be empty in session files.
+
+func TestNewLengthField(t *testing.T) {
+	t.Skip()
+}
+
+func TestNewLengthFieldUnexpectedEOF(t *testing.T) {
+	t.Skip()
+}
+
+func TestNewLengthFieldUnexpectedIdentifier(t *testing.T) {
+	t.Skip()
+}
+
+func TestLengthValue(t *testing.T) {
+	t.Skip()
+}
+
+func TestLengthString(t *testing.T) {
+	t.Skip()
+}
