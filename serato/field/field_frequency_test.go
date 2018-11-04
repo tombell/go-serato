fe9da12211d8570ee48646bac6852e4ab0bf4211
Tom Bell <tomb@tomb.io>
2018-11-04 15:13:23 +0000

    Add Frequency and unit tests

---
+++ serato/field/field_frequency_test.go
@@ -0,0 +1,25 @@
+package field_test
+
+import "testing"
+
+// XXX: Frequency field appears to always be empty in session files.
+
+func TestNewFrequencyField(t *testing.T) {
+	t.Skip()
+}
+
+func TestNewFrequencyFieldUnexpectedEOF(t *testing.T) {
+	t.Skip()
+}
+
+func TestNewFrequencyFieldUnexpectedIdentifier(t *testing.T) {
+	t.Skip()
+}
+
+func TestFrequencyValue(t *testing.T) {
+	t.Skip()
+}
+
+func TestFrequencyString(t *testing.T) {
+	t.Skip()
+}
