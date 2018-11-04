26aac397dfbd5e2a206e813f31b23bb2cbe4295a
Tom Bell <tomb@tomb.io>
2018-11-04 17:52:06 +0000

    Add Language field and unit tests

---
+++ serato/field/field_language_test.go
@@ -0,0 +1,25 @@
+package field_test
+
+import "testing"
+
+// XXX: Language field is not available as a column in history.
+
+func TestNewLanguageField(t *testing.T) {
+	t.Skip()
+}
+
+func TestNewLanguageFieldUnexpectedEOF(t *testing.T) {
+	t.Skip()
+}
+
+func TestNewLanguageFieldUnexpectedIdentifier(t *testing.T) {
+	t.Skip()
+}
+
+func TestLanguageValue(t *testing.T) {
+	t.Skip()
+}
+
+func TestLanguageString(t *testing.T) {
+	t.Skip()
+}
