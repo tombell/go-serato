10c1c07aab57abeb2a693e09628fcd09cdd416a8
Tom Bell <tomb@tomb.io>
2018-11-08 19:48:23 +0000

    Add unit test stubs

--- serato/field/fields_test.go
+++ serato/field/fields_test.go
@@ -1,3 +1,21 @@
 package field_test
 
+import "testing"
+
 // TODO: add Fields tests
+
+func TestNewFields(t *testing.T) {
+	t.Skip()
+}
+
+func TestNewFieldsWithUnusedFields(t *testing.T) {
+	t.Skip()
+}
+
+func TestNewFieldsWithUnhandledFields(t *testing.T) {
+	t.Skip()
+}
+
+func TestNewFieldsUnexpectedEOF(t *testing.T) {
+	t.Skip()
+}
