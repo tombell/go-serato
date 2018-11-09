908007308ee04a820be93a2f15d532b158bdc341
Tom Bell <tomb@tomb.io>
2018-11-09 19:26:48 +0000

    Add new test stubs and comments with details what is being tested

--- serato/field/fields_test.go
+++ serato/field/fields_test.go
@@ -17,6 +17,7 @@ func readTestDataFile(t *testing.T, filepath string) string {
 	return string(data)
 }
 
+// This will test all fields that Serato DJ Pro 2.0.5 will include values for.
 func TestNewFields(t *testing.T) {
 	str := readTestDataFile(t, "./testdata/known-fields.txt")
 	data, _ := hex.DecodeString(str)
@@ -31,14 +32,26 @@ func TestNewFields(t *testing.T) {
 	}
 }
 
-func TestNewFieldsWithUnusedFields(t *testing.T) {
+// This will test all fields that Serato DJ Pro 2.0.5 will leave empty, and not
+// include in the history file.
+func TestNewFieldsWithKnownEmptyFields(t *testing.T) {
 	t.Skip()
 }
 
-func TestNewFieldsWithUnhandledFields(t *testing.T) {
+// This will test all fields that are parsed, but we don't know what they are
+// used for.
+func TestNewFieldsWithHandledUnkownFields(t *testing.T) {
+	t.Skip()
+}
+
+// This will test that we skip passed any unknown fields which are not
+// specifically handled in code.
+func TestNewFieldsSkipsUnhandledUnknownFields(t *testing.T) {
 	t.Skip()
 }
 
 func TestNewFieldsUnexpectedEOF(t *testing.T) {
 	t.Skip()
 }
+
+// TODO: add tests, maybe table test that NewXXXField errors in NewFields
